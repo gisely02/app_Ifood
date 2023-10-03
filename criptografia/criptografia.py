81e8ce616e609299e0149a3689e2384312a7908f
rany04 <iorrany0430@gmail.com>
2023-10-03 11:22:09 -0300

    oia15

---
+++ criptografia/criptografia.py
@@ -0,0 +1,20 @@
+modo = ''
+resultado = ''
+modo = input("Você deseja:\n1 - criptografar \n2 - descriptografar: ")
+
+if(modo == '1'):
+       texto = input("Insira um texto para criptografar: ")
+
+       for i in range (0, len(texto)):
+           resultado = resultado + chr(ord(texto[i]) + 2)
+           print(resultado)
+           resultado = ''
+
+           
+if(modo == '2'):
+      texto = input("Insira um texto para descriptografar: ")
+
+      for i in range (0, len(texto)):
+            resultado = resultado + chr(ord(texto[i]) - 2)
+            print(resultado)
+            resultado = ''
